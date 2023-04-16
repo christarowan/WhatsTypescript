91cbefe2c2a8b72acad40338d36b90d6682fc07a
Christa <christa@Zipcoders-MBP-6.fios-router.home>
2023-04-16 12:01:21 -0400

    dictionary working

--- index.ts
+++ index.ts
@@ -1,23 +1,30 @@
+// Import stylesheets
 import './style.css';
 
-const form: HTMLFormElement = document.querySelector('#defineform');
 
+const form: HTMLFormElement = document.querySelector('#defineform');
+// holds baseURL for Dictionary API
+const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/';
+
+//async function handles the form 
 form.onsubmit = async () => {
   const formData = new FormData(form);
   const text = formData.get('defineword') as string;
-  
-  try {
-    const response = await fetch(`https://api.dictionaryapi.dev/api/v3/entries/en/${text}`);
-    const data = await response.json();
+  console.log(text);
+  // gets the first word user entered
 
-    // Access the definition from the API response
-    const definition = data[0]?.meaning?.noun?.[0]?.definition;
-    console.log(definition);
-    
+  try {
+    // fetch to send request to Dictionary API
+    const response = await fetch(API_URL + text);
+    // await to parse the response body as JSON
+    // and log to console
+    const data = await response.json(); 
+    console.log(data);
   } catch (error) {
-    console.error('Failed to fetch word definition:', error);
+    // handles any errors during API request 
+    console.error(error); 
   }
+
 
   return false; // prevent reload
 };
-
